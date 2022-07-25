f6051ba4581e70e4b215401d681ce056038e30ae
L2M <loren.milliman@gmail.com>
2022-07-25 16:31:33 -0700

    add resources and data flat file

---
+++ shared/squatches.js
@@ -0,0 +1,65 @@
+export const SQUATCHES = [
+  {
+    id: 0,
+    name: "Harry",
+    image: require("../assets/images/squatch-0.jpg"),
+    age: 234,
+    likes: ["rocks", "trees", "scissors"],
+    tree: "hemlock",
+    description: "Not all humans are bad."
+  },
+  {
+    id: 1,
+    name: "Fuzzy Wuzzy",
+    image: require("../assets/images/squatch-1.jpg"),
+    age: 234,
+    likes: ["grooming", "night strolls", "rocks"],
+    tree: "fir",
+    description: "I like to climb trees and do fuzzy photo bombs"
+  },
+  {
+    id: 2,
+    name:"Yeti",
+    image: require("../assets/images/squatch-2.jpg"),
+    age: 451,
+    likes: ["gazing at the stars", "meditation"],
+    tree: "pine",
+    description: "No one every accused me of being hasty"
+  },
+  {
+    id: 3,
+    name:"Yeren",
+    image: require("../assets/images/squatch-3.jpg"),
+    age: 360,
+    likes: ["playing in the dirt", "hiking", "bike riding"],
+    tree: "fir",
+    description: "I usually keep to myself, but that can be lonly sometimes"
+  },
+  {
+    id: 4,
+    name:"Bobo",
+    image: require("../assets/images/squatch-4.jpg"),
+    age: 314,
+    likes: ["pranks", "mischief", "nonsense"],
+    tree: "cedar",
+    description: "It wasn't me, regardless of what you might of been told"
+  },
+  {
+    id: 5,
+    name:"Yowie",
+    image: require("../assets/images/squatch-5.jpg"),
+    age: 360,
+    likes: ["hair", "growling", "scaring hikers"],
+    tree: "birch",
+    description: "I like to hang out near trails, and jump out of the bushes as hikers go by"
+  },
+  {
+    id: 6,
+    name:"Groot",
+    image: require("../assets/images/squatch-6.jpg"),
+    age: 909,
+    likes: ["groot", "groot", "groot"],
+    tree: "aspen",
+    description: "groot groot groot groot groot!"
+  }
+];
