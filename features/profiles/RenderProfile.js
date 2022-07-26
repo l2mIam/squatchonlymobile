63b108eab6b19fe13e86d22f97462af832174788
L2M <loren.milliman@gmail.com>
2022-07-26 00:11:22 -0700

    fix navigation error, add image height to profile

--- features/profiles/RenderProfile.js
+++ features/profiles/RenderProfile.js
@@ -1,16 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View, Dimensions } from "react-native";
 import { Card } from "react-native-elements";
 
 const RenderProfile = ({ squatch }) => {
   if (squatch) {
+    const win = Dimensions.get('window');
+
     return (
       <Card containerStyle={{ padding: 0 }}>
-        <Card.Image source={squatch.image}>
-          <View style={{ justifyContent: 'center', flex: 1 }}>
-            {/* <Text style={{ color: 'white', textAlign: 'center', fontSize: 20 }}>
-              {squatch.name}
-            </Text> */}
-          </View>
+        <Card.Image
+          source={squatch.image}
+          style={{
+            height: win.width *2/3
+          }}
+        >
         </Card.Image>
         <Text style={{ margin: 20 }}>{`Age: ${squatch.age}\nFavorite Tree: ${squatch.tree}\n${squatch.description}`}</Text>
       </Card>
